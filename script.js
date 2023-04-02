7378a5866de59bb3e38ba09b40b8193f04c4dfcf
tejaz10 <116888081+tejaz10@users.noreply.github.com>
2023-04-02 17:55:24 +0530

    version 1

---
+++ script.js
@@ -0,0 +1,26 @@
+const rem = document.querySelector(".rem");
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+btnPopup.addEventListener("click", () => {
+  rem.classList.remove("main");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
